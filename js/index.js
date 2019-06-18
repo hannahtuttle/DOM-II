98dfc098e0c5cb32c6a09563d9ebc87e5e295017
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-06-18 14:41:50 -0700

    6 event listeners, 4 to go

--- js/index.js
+++ js/index.js
@@ -15,6 +15,19 @@ navBtn[3].addEventListener('mouseover', event => {
 //     event.target.style.color = 'blue';
 // }))
 
+navBtn[0].addEventListener('mouseleave', event => {
+    event.target.style.color = 'black';
+})
+navBtn[1].addEventListener('mouseleave', event => {
+    event.target.style.color = 'black';
+})
+navBtn[2].addEventListener('mouseleave', event => {
+    event.target.style.color = 'black';
+})
+navBtn[3].addEventListener('mouseleave', event => {
+    event.target.style.color = 'black';
+})
+
 const photo = document.querySelectorAll('.img-content')
 console.log(photo)
 photo[0].addEventListener('dblclick', event => {
@@ -37,17 +50,28 @@ buttons[2].addEventListener('click', (event) => {
     event.target.style.backgroundColor = 'blue';
 })
 
-const funBus = document.querySelector('.nav-container')
-funBus.addEventListener('focus', (event) => {
-    event.target.style.background = 'orange'
-})
+// const funBus = document.querySelector('.nav-container')
+// funBus.addEventListener('focus', (event) => {
+//     event.target.style.background = 'orange'
+// })
 
 const funBusPic = document.querySelector('.intro img')
 funBusPic.addEventListener('contextmenu', event => {
     event.preventDefault();
 })
 
-const destinationPic = document.querySelector('.content-destination img')
-destinationPic.addEventListener('keydown', event => {
-    event.target.style.display = 'none'
+// const destinationPic = document.querySelector('.content-destination img')
+// destinationPic.addEventListener('wheel', event => {
+//     event.target.style.display = 'none';
+// })
+
+// const display  = document.querySelectorAll('.destination')
+// display[0].addEventListener('resize', event => {
+//     console.log('window size')
+// })
+
+const msDown = document.querySelector('h1')
+msDown.addEventListener('mousedown', event => {
+    event.target.style.color = 'blue';
 })
+
