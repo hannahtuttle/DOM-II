ff79904b07cf74decfba8c099eaff5907f966c21
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-06-18 14:09:10 -0700

    4 working event listeners, 2 not working

--- js/index.js
+++ js/index.js
@@ -1,23 +1,53 @@
 // Your code goes here
 
-const navBtn = document.querySelectorAll ('#navBtn')
-//console.log(navBtn)
-navBtn.addEventListener('mouseover', event => {
-    event.target.style.color = 'blue'
+const navBtn = document.querySelectorAll('.nav-link')
+navBtn[0].addEventListener('mouseover', event => {
+    event.target.style.color = 'blue';})
+navBtn[1].addEventListener('mouseover', event => {
+        event.target.style.color = 'blue';})
+navBtn[2].addEventListener('mouseover', event => {
+            event.target.style.color = 'blue';})
+navBtn[3].addEventListener('mouseover', event => {
+                event.target.style.color = 'blue';})
+
+
+// navBtn.forEach(item => item.addEventListener('mouseover', event => {
+//     event.target.style.color = 'blue';
+// }))
+
+const photo = document.querySelectorAll('.img-content')
+console.log(photo)
+photo[0].addEventListener('dblclick', event => {
+    event.target.style.transform = 'scale(1.4)'
+})
+photo[1].addEventListener('dblclick', event => {
+    event.target.style.transform = 'scale(1.4)'
 })
 
 
-// const navItems = document.querySelectorAll('.nav-link');
 
-// navItems.forEach((gc) => gc.style.color = 'red')
+const buttons = document.querySelectorAll('.btn')
+buttons[0].addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'blue';
+})
+buttons[1].addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'blue';
+})
+buttons[2].addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'blue';
+})
 
+const funBus = document.querySelector('.nav-container')
+funBus.addEventListener('focus', (event) => {
+    event.target.style.background = 'orange'
+})
 
-// function wheel(event) {
-//     event.preventDefault();
-//     scale += event.deltaY * -0.01;
-//     el.style.transform = `scale(${scale})`
-// }
-// let scale = 1;
+const funBusPic = document.querySelector('.intro img')
+funBusPic.addEventListener('contextmenu', event => {
+    event.preventDefault();
+})
 
-// const txContent = ducument.querySelectorAll('.text-content')
-// el.onwheel = zoom;
+const destinationPic = document.querySelector('.content-destination img')
+destinationPic.addEventListener('keydown', event => {
+    event.target.style.display = 'none'
+})
