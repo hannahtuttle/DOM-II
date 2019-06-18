c9fb0210d8da9bf9afb070e23da2323be9cdaf15
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-06-18 12:51:48 -0700

    having a hard time getting anything to work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+const navBtn = document.querySelectorAll ('#navBtn')
+//console.log(navBtn)
+navBtn.addEventListener('mouseover', event => {
+    event.target.style.color = 'blue'
+})
+
+
+// const navItems = document.querySelectorAll('.nav-link');
+
+// navItems.forEach((gc) => gc.style.color = 'red')
+
+
+// function wheel(event) {
+//     event.preventDefault();
+//     scale += event.deltaY * -0.01;
+//     el.style.transform = `scale(${scale})`
+// }
+// let scale = 1;
+
+// const txContent = ducument.querySelectorAll('.text-content')
+// el.onwheel = zoom;
