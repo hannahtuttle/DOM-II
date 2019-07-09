74f5b476d7000a22d82223d63f2aa1cee28b0397
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-07-09 15:11:03 -0700

    finished the transition, ordering of ricket boxed needs work

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,19 +1,26 @@
 
 
 let box = document.querySelectorAll('.block')
-console.log(box)
+//console.log(box)
 let arrBox = Array.from(box)
 console.log(arrBox)
 
 arrBox.forEach((n) => {
     n.addEventListener('click', event => {
+        //event.target.style.transition = '10s'
         event.target.style.order = '1';
         console.log('click', event)
 })})
 
 arrBox.forEach((n) => {
     n.addEventListener('mousedown', event => {
-        event.target.style.transition = '3s'
+        event.target.style.transition = '10s'
         event.target.style.marginLeft = '900%'
         console.log('mousedown', event)
+})})
+
+arrBox.forEach((n) => {
+    n.addEventListener('mouseup', event => {
+        event.target.style.marginLeft = '10px'
+        console.log('mouseup', event)
 })})
